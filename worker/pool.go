938622fd437cc011fb8dfe40438c25edc24cc6e3
Rodrigo Campos <rodrigo.redcode@gmail.com>
2025-05-13 00:32:14 -0300

    Add worker module
    Adds an initial module to accomodate worker features. In this initial
    version it implements a worker Pool where it is possilbe to define a
    limit for concurrency tasks.

---
+++ worker/pool.go
@@ -0,0 +1,42 @@
+package worker
+
+import "sync"
+
+// Pool is a pool of goroutines which can run tasks concurrently
+type Pool struct {
+	size      int
+	waitGroup sync.WaitGroup
+	tasks     chan Task
+}
+
+// NewPool creates a worker pool with the given size
+func NewPool(size int) *Pool {
+	p := &Pool{
+		size:  size,
+		tasks: make(chan Task, 0),
+	}
+	p.spawn()
+	return p
+}
+
+// spawn spawns n goroutines where n is the size of the pool
+func (p *Pool) spawn() {
+	for i := 0; i < p.size; i++ {
+		go func() {
+			defer p.waitGroup.Done()
+			task := <-p.tasks
+			task()
+		}()
+	}
+}
+
+// Submit submits a new task for the workers to run
+func (p *Pool) Submit(task Task) {
+	p.waitGroup.Add(1)
+	p.tasks <- task
+}
+
+// WaitAll waits until every task submited has finished
+func (p *Pool) WaitAll() {
+	p.waitGroup.Wait()
+}
